4154d1dffa76ed0fae332244a7c473b7a8d5e1c5
deluxturtle <sebaturtle@gmail.com>
2015-11-02 11:51:08 -0600

    Merged Lab-06 to projet6

---
+++ Project-06/Assets/Scripts/Multiplayer/Script_PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Script_PlayerHealth : NetworkBehaviour {
+
+    float seconds = 0;
+    float health = 100;
+
+	
+	// Update is called once per frame
+	void Update () {
+        seconds += Time.deltaTime;
+        if(seconds > 2)
+        {
+            seconds = 0;
+            Debug.Log(health);
+        }
+	}
+}
